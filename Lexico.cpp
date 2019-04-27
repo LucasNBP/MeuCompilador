72862bca71b2f12aed692bd81e21d496d8cd9d35
LucasNatanael <lucas.natan.prates@hotmail.com>
2019-04-26 21:07:43 -0300

    completando simbolos reservados

--- Lexico.cpp
+++ Lexico.cpp
@@ -9,14 +9,18 @@ using namespace std;
 struct Tokens{
 	string tokenValue;
 	string tokenName;
+	bool reservedWord=false;
 };
 
+//estrutura para receber o retorno informado pelos automatos
 struct valoresRetorno{
 	int i;
 	int contadorColuna;
 	int contadorLinha;
 	Tokens tokenGerado;
 };
+
+//faltam simbolos " & | !
 
 /*
 Reserved words:
@@ -25,6 +29,7 @@ Reserved words:
 	$<= $>= $= $<> $" $& $| $!
 */
 
+//automato para trabalhar iniciando com <
 valoresRetorno gerarTokenMenor(char leitura[Tam_Maximo], int i, int contadorColuna, int contadorLinha){
 	valoresRetorno retornos;
 	Tokens tokenGerado;
@@ -35,25 +40,27 @@ valoresRetorno gerarTokenMenor(char leitura[Tam_Maximo], int i, int contadorColu
 	contadorColuna++;
 	switch(leitura[i])
 	{
-		case '=':{
+		case '=':{ //menor igual
 			acumularToken+=leitura[i];
 			tokenGerado.tokenValue=acumularToken;
 			tokenGerado.tokenName="<=";
 		}break;
 
-		case '>':{
+		case '>':{ //diferente
 			acumularToken+=leitura[i];
 			tokenGerado.tokenValue=acumularToken;
 			tokenGerado.tokenName="<>";
 		}break;
 
-		case '-':{
+		case '-':{ //atribuicao
 			acumularToken+=leitura[i];
 			tokenGerado.tokenValue=acumularToken;
 			tokenGerado.tokenName="<-";
 		}break;
 
-		default :{
+		default :{ //menor p/ qualquer outro char
+			i--;
+			contadorColuna--;
 			tokenGerado.tokenValue=acumularToken;
 			tokenGerado.tokenName="<";
 		}break;
@@ -67,6 +74,7 @@ valoresRetorno gerarTokenMenor(char leitura[Tam_Maximo], int i, int contadorColu
 	return retornos;
 }
 
+//automato para trabalhar iniciando com >
 valoresRetorno gerarTokenMaior(char leitura[Tam_Maximo], int i, int contadorColuna, int contadorLinha){
 	valoresRetorno retornos;
 	Tokens tokenGerado;
@@ -77,14 +85,16 @@ valoresRetorno gerarTokenMaior(char leitura[Tam_Maximo], int i, int contadorColu
 	contadorColuna++;
 	switch(leitura[i])
 	{
-		case '=':{
+		case '=':{ //maior igual
 			acumularToken+=leitura[i];
 			tokenGerado.tokenValue=acumularToken;
 			tokenGerado.tokenName=">=";
 			break;
 		}break;
 
-		default :{
+		default :{ //maior p/ qualquer outro char
+			i--;
+			contadorColuna--;
 			tokenGerado.tokenValue=acumularToken;
 			tokenGerado.tokenName=">";
 		}break;
@@ -98,27 +108,56 @@ valoresRetorno gerarTokenMaior(char leitura[Tam_Maximo], int i, int contadorColu
 	return retornos;
 }
 
+//automato para trabalhar iniciando com *
+valoresRetorno gerarTokenMulExpo(char leitura[Tam_Maximo], int i, int contadorColuna, int contadorLinha){
+	valoresRetorno retornos;
+	Tokens tokenGerado;
+	string acumularToken="";
+	acumularToken+=leitura[i];
+
+	i++;
+	contadorColuna++;
+	switch(leitura[i])
+	{
+		case '*':{ //expoente
+			acumularToken+=leitura[i];
+			tokenGerado.tokenValue=acumularToken;
+			tokenGerado.tokenName="**";
+			break;
+		}break;
+
+		default :{ //* p/ qualquer outro char
+			i--;
+			contadorColuna--;
+			tokenGerado.tokenValue=acumularToken;
+			tokenGerado.tokenName="*";
+		}break;
+	}
+
+	retornos.i=i;
+	retornos.contadorColuna=contadorColuna;
+	retornos.contadorLinha=contadorLinha;
+	retornos.tokenGerado=tokenGerado;
+
+	return retornos;
+}
+
+//estrutura principal
 int main(){
 	ios_base::sync_with_stdio(false);
 
-	char leitura[Tam_Maximo];
-	int tamanhoLeitura;
-	bool flagParar=false;	
+	char leitura[Tam_Maximo]; //variavel que recebera o codigo
+	int tamanhoLeitura; //variavel com o tamanho da entrada dada
+	bool flagParar=false; //flag para saber se continuo as analises ou nao
 	int contadorColuna=0;
 	int contadorLinha=1;
 	string acumularToken="";
-	Tokens tokenGerado;
-	valoresRetorno retornos;
-
-	vector<Tokens> listaTokens;
-	
+	Tokens tokenGerado; //variavel para juntar a leitura em um token
+	valoresRetorno retornos; //variavel para receber o retorno informado pelos automatos
+	vector<Tokens> listaTokens; //lista de tokens gerados
 	tamanhoLeitura = fread(leitura, sizeof(char), Tam_Maximo, stdin);
 	
-	/*for(int i=0; i<tamanhoLeitura; i++){
-		cout<<(int)leitura[i]<<"\n";
-	}
-	cout<<"-------------";*/
-	
+	//conferindo se todos os caracteres pertencem as especificacoes
 	for(int i=0; i<tamanhoLeitura; i++){
 		if(leitura[i]>126 || leitura[i]<9 || (leitura[i]>10 && leitura[i]<32)){
 			cout<<"ARQUIVO INVALIDO\n";
@@ -130,7 +169,8 @@ int main(){
 	if(flagParar){
 		return 0;
 	}
-
+	
+	//analise lexica
 	for(int i=0; i<tamanhoLeitura; i++){
 		contadorColuna++;
 		switch (leitura[i])
@@ -140,9 +180,20 @@ int main(){
 				contadorColuna=0;
 			}break;
 
-			case '=':{
-				tokenGerado.tokenName="=";
-				tokenGerado.tokenValue="=";
+			case('='): case('.'): case(':'): case(';'): case('+'): case('-'): case('/'): case('%'): case('('): case(')'): case('['): case(']'):{
+				tokenGerado.tokenName=leitura[i];
+				tokenGerado.tokenValue=leitura[i];
+				tokenGerado.reservedWord=true;
+				listaTokens.push_back(tokenGerado);
+			}break;
+			
+			case '*':{
+				retornos=gerarTokenMulExpo(leitura, i, contadorColuna, contadorLinha);
+				i=retornos.i;
+				contadorLinha=retornos.contadorLinha;
+				contadorColuna=retornos.contadorColuna;
+				tokenGerado=retornos.tokenGerado;
+				tokenGerado.reservedWord=true;
 				listaTokens.push_back(tokenGerado);
 			}break;
 
@@ -152,6 +203,7 @@ int main(){
 				contadorLinha=retornos.contadorLinha;
 				contadorColuna=retornos.contadorColuna;
 				tokenGerado=retornos.tokenGerado;
+				tokenGerado.reservedWord=true;
 				listaTokens.push_back(tokenGerado);
 			}break;
 
@@ -161,9 +213,9 @@ int main(){
 				contadorLinha=retornos.contadorLinha;
 				contadorColuna=retornos.contadorColuna;
 				tokenGerado=retornos.tokenGerado;
+				tokenGerado.reservedWord=true;
 				listaTokens.push_back(tokenGerado);
-			}
-
+			}break;
 		}
 	}
 
@@ -171,10 +223,5 @@ int main(){
 		cout<<"\n"<<listaTokens[i].tokenName<<"\n";
 	}
 	
-	
-	/*
-	string teste = "lucas natanael\nlucas natanael";
-	cout<<teste;
-	*/
 	return 0;
 }
